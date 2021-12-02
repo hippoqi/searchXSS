8c3f83afe33697dd9b3fa29eabe02c58e0af318d
Shiqi <sohsq.shiqi@gmail.com>
2021-12-02 18:51:51 +0800

    add

--- app.py
+++ app.py
@@ -7,13 +7,8 @@ def hello():
     if request.method == "POST":
         formsearch = request.form.get('txtsearch')
         print(formsearch)
-    return render_template('input_pass.html')
+    return render_template('index.html')
 
-def sample_sum_func(a = 0, b = 0):
-    return a + b
-
-def sample_sub_func(a = 0, b = 0):
-    return a - b
 
 if __name__ == '__main__':
     app.run()
